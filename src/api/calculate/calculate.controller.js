453b4f09768c639b2299b0627b01c423788bf73d
Joe Brady <joe.brady@uk.ibm.com>
2018-04-25 17:40:07 +0100

    Update risk algorithm and rules for new trial approach

--- src/api/calculate/calculate.controller.js
+++ src/api/calculate/calculate.controller.js
@@ -1,15 +1,14 @@
-const { hygienePotentialHazardService } = require('../../services/');
-const store = require('../../store');
-const winston = require('winston');
+const { hygienePotentialHazardService } = require("../../services/");
+const store = require("../../store");
+const winston = require("winston");
 
-module.exports = async (answerIds) => {
-  winston.info('calculate controller called');
+module.exports = async answerIds => {
+  winston.info("calculate controller called");
 
   try {
     const result = await hygienePotentialHazardService.calculateRisk(answerIds);
     return result;
-  }
-  catch (err) {
+  } catch (err) {
     winston.error(`calculate controller error: ${err}`);
     return err;
   }
