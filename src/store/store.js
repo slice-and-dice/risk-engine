fed59ae3c4ce050488375dfdfbc62b2f352b2321
Joe Brady <joe.brady@uk.ibm.com>
2018-04-26 11:42:18 +0100

    Add complete list of business types

--- src/store/store.js
+++ src/store/store.js
@@ -3,18 +3,45 @@ const store = {
     "Farm-Fruit and vegetable grower": {
       answerIds: ["TYPE-A0"],
       baseScores: {
-        A: 30,
+        A: 0,
         B: 0,
-        C: 5,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm-Livestock": {
+      answerIds: ["TYPE-A0"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm-Arable": {
+      answerIds: ["TYPE-A0"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm-Other": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
         D: 0
       }
     },
     Beekeeper: {
       answerIds: ["TYPE-A0", "TYPE-A3"],
       baseScores: {
-        A: 30,
+        A: 0,
         B: 0,
-        C: 20,
+        C: 0,
         D: 0
       }
     },
@@ -28,9 +55,972 @@ const store = {
         "TYPE-A11"
       ],
       baseScores: {
-        A: 30,
-        B: 10,
-        C: 20,
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Hunting and trapping": {
+      answerIds: ["TYPE-A0", "TYPE-A3", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Egg producer-Egg packer": {
+      answerIds: ["TYPE-A0", "TYPE-A9", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Egg producer-Other": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Fishing vessel-Finfish": {
+      answerIds: ["TYPE-A0", "TYPE-A3", "TYPE-A9", "TYPE-A12", "TYPE-A13"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Fishing vessel-Shellfish": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A3",
+        "TYPE-A5",
+        "TYPE-A9",
+        "TYPE-A11",
+        "TYPE-A12",
+        "TYPE-A13"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Fishing vessel-Other": {
+      answerIds: ["TYPE-A0", "TYPE-A9", "TYPE-A11", "TYPE-A12", "TYPE-A13"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farmed fishing-Finfish": {
+      answerIds: ["TYPE-A0", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farmed fishing-Shellfish": {
+      answerIds: ["TYPE-A0"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farmed fishing-Other": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Processing and preserving of meat": {
+      answerIds: ["TYPE-A0", "TYPE-A3", "TYPE-A9", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    Abattoirs: {
+      answerIds: ["TYPE-A0", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Processing and preserving of fish, crustaceans and molluscs": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Purification centres for shellfish": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A3",
+        "TYPE-A9",
+        "TYPE-A11",
+        "TYPE-A12",
+        "TYPE-A13"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Processing and preserving of potatoes": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture of fruit and vegetable juice": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A2",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A10",
+        "TYPE-A11"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Other processing and preserving of fruit and vegetables": {
+      answerIds: ["TYPE-A0", "TYPE-A3", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture of oils and fats": {
+      answerIds: ["TYPE-A6", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Operation of dairies and cheese making": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A10",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture of ice cream": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A10",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Bakeries with no on-site retail": {
+      answerIds: ["TYPE-A0", "TYPE-A3"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Bakeries  selling through own shops": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A10",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture of prepared meals and dishes": {
+      answerIds: ["TYPE-A0", "TYPE-A3", "TYPE-A9", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture of homogenised food preparations and dietetic food": {
+      answerIds: ["TYPE-A1", "TYPE-A3", "TYPE-A9", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture of other food products n.e.c.": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture-Alcoholic drinks": {
+      answerIds: ["TYPE-A0", "TYPE-A3"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Manufacture-Soft drinks, mineral waters and other bottled waters": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Packer-Mineral waters": {
+      answerIds: ["TYPE-A0", "TYPE-A2"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Packer-Contract packers": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Food delivery service-Deliver food to consumers": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Food delivery service-Process food order only for consumers, e.g. Just Eat": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Food delivery service-Other": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Storage provider": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Food broker": {
+      answerIds: ["TYPE-A0"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    Wholesaler: {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Cash and carry": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A3",
+        "TYPE-A9",
+        "TYPE-A11",
+        "TYPE-A12",
+        "TYPE-A13"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Haulage company": {
+      answerIds: ["TYPE-A0"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Internet only food": {
+      answerIds: [
+        "TYPE-A0",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A9",
+        "TYPE-A10",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Supermarket/Hypermarket": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A9",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm shops-Gate sales": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A9",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm shops-Meals made from scratch": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A9",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm shops-Reheating and serving food": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm shops-Providing snack and drink only": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm shops-Retail only": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A9",
+        "TYPE-A11"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Farm shops-Other": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Confectionery/sweet shop": {
+      answerIds: ["TYPE-A1", "TYPE-A5"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Butcher (retail only)": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A9",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    Fishmonger: {
+      answerIds: ["TYPE-A1", "TYPE-A9", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Greengrocer/fruiterer": {
+      answerIds: ["TYPE-A1", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Health food shop": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Bakers shop (retail only)": {
+      answerIds: ["TYPE-A1", "TYPE-A3", "TYPE-A4", "TYPE-A5", "TYPE-A6"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Newsagent, post office": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Market stalls (retail only)": {
+      answerIds: ["TYPE-A1", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Off licence": {
+      answerIds: ["TYPE-A1", "TYPE-A5"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Petrol/Garage - mini supermarket": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    Delicatessen: {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A9",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    Chemist: {
+      answerIds: ["TYPE-A1", "TYPE-A5"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Retailers - shelf stable only": {
+      answerIds: ["TYPE-A1", "TYPE-A5"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Vending machine": {
+      answerIds: ["TYPE-A1", "TYPE-A5"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Retailer - others": {
+      answerIds: ["TYPE-A1", "TYPE-A9"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Fast food restaurant": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Other-Providing meals": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Other-Providing snacks and drink only": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Hotels - Snacks": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A6", "TYPE-A7"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Hotels - Meals": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Hotels - Breakfast": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Pubs - Meals": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Pubs - snacks and drinks only": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A6", "TYPE-A7"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Take away-No food consumed on site ": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A11",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Nursing, care homes, day centres": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    Hospitals: {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Child minder-Meals": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A10",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Child minder-Snack and drink only": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A6", "TYPE-A7"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Childcare, Nursery; pre/after school care; play group etc-Meals": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Childcare, Nursery; pre/after school care; play group etc-Snack and drink only": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A6", "TYPE-A7"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "School -Meals": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "School snacks and drinks only": {
+      answerIds: ["TYPE-A1", "TYPE-A5", "TYPE-A6", "TYPE-A7"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Mobile retailer": {
+      answerIds: ["TYPE-A1", "TYPE-A4", "TYPE-A8", "TYPE-A9", "TYPE-A12"],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Mobile caterer": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A8",
+        "TYPE-A10",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Market vehicles & stalls": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A9",
+        "TYPE-A10",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Movable food establishment": {
+      answerIds: [
+        "TYPE-A1",
+        "TYPE-A3",
+        "TYPE-A4",
+        "TYPE-A5",
+        "TYPE-A6",
+        "TYPE-A7",
+        "TYPE-A8",
+        "TYPE-A12"
+      ],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
+        D: 0
+      }
+    },
+    "Contract caterer": {
+      answerIds: [],
+      baseScores: {
+        A: 0,
+        B: 0,
+        C: 0,
         D: 0
       }
     }
